e7574fa1191639630800bb2db2377ccb1e933a68
Amanuel Tamrat <56165039+zapermaper@users.noreply.github.com>
2024-02-02 00:46:50 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         // Scanner in = new Scanner(System.in);
-        Scanner in = new Scanner(new File("input.dat"));
+        Scanner in = new Scanner(new File("javaTemp\\input.dat"));
         int test=in.nextInt();in.nextLine();while(test-->0)solve(in);//4 test cases/comment out if no need
 
     }
